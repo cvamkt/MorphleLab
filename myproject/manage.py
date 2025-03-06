06409de0a0c1795f315638f6270b22f2f360cd88
shivam kumar <154726545+cvamkt@users.noreply.github.com>
2025-03-06 15:04:41 +0000

    "morphle "

---
+++ myproject/manage.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import os
+import datetime
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/htop')
+def htop():
+    full_name = "shivam Kumar"
+    username = os.getenv("USER", "codespace")
+    server_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    top_output = subprocess.getoutput("top -b -n 1")
+    
+    
+    
+    return f"""
+<pre>
+Name: {full_name}
+Username: {username}
+Server Time (IST): {server_time}
+TOPoutput:
+    
+
+{top_output}
+<pre>
+"""
+    
+    if _name_ == '_main_':
+        first.run(host='0.0.0.0', port=5000)
